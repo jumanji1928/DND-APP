a452517ed85ea508645a2106d7f7de940e3e88f5
Matthew Janczak <mjhawk28@yahoo.com>
2023-01-11 22:14:42 -0600

    NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,12 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
